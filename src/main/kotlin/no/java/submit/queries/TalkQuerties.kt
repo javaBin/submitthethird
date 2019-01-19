0d87791a6b2f1250dc51d13c2fbf9630ad7f1dfa
Anders Karlsen <karlsenanders@gmail.com>
2019-01-19 15:27:20 +0100

    Sorted conferences newest first

--- src/main/kotlin/no/java/submit/queries/TalkQuerties.kt
+++ src/main/kotlin/no/java/submit/queries/TalkQuerties.kt
@@ -21,7 +21,7 @@ fun allTalksForSpeaker(callIdentification: CallIdentification):JsonObject {
     val jsonObject = SleepingPillService.get("/data/submitter/${URLEncoder.encode(callIdentification.callerEmail,"UTF-8")}/session")
     val talks:List<Talk> = jsonObject.arrayValue("sessions").orElse(JsonArray()).objectStream().map(::Talk).collect(Collectors.toList())
     val talksMapped:Map<String?,List<Talk>> = talks.groupBy { it.conferenceId }
-    val mappedList:List<Conference> = ConferenceService.allConferences.filter { talksMapped.containsKey(it.id) }.map { Conference(it.name,talksMapped[it.id]!!) }
+    val mappedList:List<Conference> = ConferenceService.allConferences.filter { talksMapped.containsKey(it.id) }.map { Conference(it.name,talksMapped[it.id]!!) }.sortedByDescending { it.conferenceName }
     val objectList:List<JsonObject> = mappedList.map { JsonGenerator.generate(it) as JsonObject }
     return JsonObject().put("conferences",JsonArray.fromNodeList(objectList))
 }
