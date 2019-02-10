89d7d03f62aff7c8b4f886dedeb5e0493ffe6af9
Anders Karlsen <karlsenanders@gmail.com>
2019-02-10 10:20:16 +0100

    Updated to prodconfig

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 mvn clean install
-ansible-vault decrypt conf/testconf.encrypted --output=config.txt
+ansible-vault decrypt conf/prodconf.encrypted --output=config.txt
 cp target/submitthethird-1.0-SNAPSHOT-jar-with-dependencies.jar app.jar
 jar uf app.jar config.txt
 rm config.txt
