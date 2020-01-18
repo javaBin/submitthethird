60abd315344e6f7b27f9908956e93bb7178d3a43
Anders Karlsen <karlsenanders@gmail.com>
2020-01-18 15:11:50 +0100

    Config updates

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,7 @@ ansible-vault decrypt conf/prodconf.encrypted --output=config.txt
 cp target/submitthethird-1.0-SNAPSHOT-jar-with-dependencies.jar app.jar
 jar uf app.jar config.txt
 rm config.txt
-eb deploy
+zip -r app.zip app.jar Procfile
 rm app.jar
+eb deploy
+rm app.zip
