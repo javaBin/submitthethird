61e2cdf67533b9cf17381f4e41500a8cd6d0df40
Anders Karlsen <karlsenanders@gmail.com>
2020-01-18 14:56:01 +0100

    Config update for aws

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ ansible-vault decrypt conf/prodconf.encrypted --output=config.txt
 cp target/submitthethird-1.0-SNAPSHOT-jar-with-dependencies.jar app.jar
 jar uf app.jar config.txt
 rm config.txt
-gcloud app deploy app.yaml
+eb deploy
 rm app.jar
